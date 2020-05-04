02aacd9be34ce5e37f0e0f60c7d3241a1837d071
Francisco Lúcio <flrda.elt@uea.edu.br>
2020-05-04 15:08:12 -0400

    Criados atributos complexo: Filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade = 33):
+    def __init__(self, *filhos, nome=None, idade = 33):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Lúcio'
-    print(p.nome)
+    lucio = Pessoa(nome ='Lucio')
+    luciano = Pessoa(lucio, nome = 'Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)

4aaecba326115eed10d1587c5e9ddf221ae46068
Francisco Lúcio <flrda.elt@uea.edu.br>
2020-05-08 17:38:56 -0400

    Implementada a direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,15 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 33
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} -olhos {cls.olhos}'
+
+
 if __name__ == '__main__':
     lucio = Pessoa(nome ='Lucio')
     luciano = Pessoa(lucio, nome = 'Luciano')
@@ -27,3 +36,5 @@ if __name__ == '__main__':
     print(Pessoa.olhos)
     print(luciano.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(lucio.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
